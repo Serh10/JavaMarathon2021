fcbcb93cf7c6f96c53446418aac1d6bcabb929b0
serh10 <v1nograd@inbox.ru>
2021-12-27 23:43:07 +0300

    complete day7 tasks2

---
+++ src/main/java/day7/Player.java
@@ -0,0 +1,47 @@
+package day7;
+
+public class Player {
+    private int stamina;
+    public static final int MAX_STAMINA = 100;
+    public static final int MIN_STAMINA = 0;
+    private static int countPlayers;
+
+    public Player(int stamina) {
+        this.stamina = stamina;
+        if (countPlayers < 6) {
+            countPlayers++;
+        }
+
+    }
+
+    public int getStamina() {
+        return stamina;
+    }
+
+    public static int getCountPlayers() {
+        return countPlayers;
+    }
+
+    public void run() {
+        if (stamina > 0) {
+            stamina--;
+        } else if (stamina == 0) {
+            System.out.println("Игроку нужен отдых, он убывает ");
+            countPlayers--;
+            return;
+        }
+
+    }
+
+    public void info() {
+        if (countPlayers < 6) {
+            System.out.println("Команды неполные. На поле еще есть " + (6 - countPlayers) + " свободных мест");
+        } else {
+            System.out.println("На поле нет свободных мест");
+        }
+    }
+
+    public void playersOnPole() {
+        System.out.println("Игроков на поле: " + countPlayers);
+    }
+}
